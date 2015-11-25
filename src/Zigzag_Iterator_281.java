a23522909276107717e1b1371edb29763114ec7b
zs <zs@zs-PC>
2015-11-25 00:12:40 -0500

    2

--- src/Zigzag_Iterator_281.java
+++ src/Zigzag_Iterator_281.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /*Given two 1d vectors, implement an iterator to return their elements alternately.
@@ -8,47 +10,26 @@ By calling next repeatedly until hasNext returns false, the order of elements re
 Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?*/
 public class Zigzag_Iterator_281 {
 	
-	// can use iterator and use a flag to denote which array should  iterator through,
-	// this code is not good.
-	private List<Integer> v1;
-    private List<Integer> v2;
-    private int i;
-    private int j;
-    private int listId;
- 
-    public Zigzag_Iterator_281(List<Integer> v1, List<Integer> v2) {
-        this.v1 = v1;
-        this.v2 = v2;
-        this.i = 0;
-        this.j = 0;
-        this.listId = 0;
-    }
- 
-    public int next() {
-        int result = 0;
-        if (i >= v1.size()) {
-            result = v2.get(j);
-            j++;
-        } else if (j >= v2.size()) {
-            result = v1.get(i);
-            i++;
-        } else {
-            if (listId == 0) {
-                result = v1.get(i);
-                i++;
-                listId = 1;
-            } else {
-                result = v2.get(j);
-                j++;
-                listId = 0;
-            }
-        }
-         
-        return result;
-    }
- 
-    public boolean hasNext() {
-        return i < v1.size() || j < v2.size();
-    }
+	List<Iterator<Integer> > iters = new ArrayList<Iterator<Integer> >();   
+    
+    int count = 0;  
+  
+    public Zigzag_Iterator_281(List<Integer> v1, List<Integer> v2) {  
+        if( !v1.isEmpty() ) iters.add(v1.iterator());  
+        if( !v2.isEmpty() ) iters.add(v2.iterator());  
+    }  
+  
+    public int next() {  
+        int x = iters.get(count).next();  
+        if(!iters.get(count).hasNext()) iters.remove(count);  
+        else count++;  
+          
+        if(iters.size()!=0) count %= iters.size();  
+        return x;  
+    }  
+  
+    public boolean hasNext() {  
+        return !iters.isEmpty();  
+    }  
 	
 }
